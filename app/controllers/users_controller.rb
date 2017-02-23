1945e9233335c1a6af39974223f2778dec100213
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-02-23 13:23:42 -0500

    added_create_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
 
 		redirect_to "/users"
 	end
+
+	def new
+	end
+
+	def create
+		user = User.new
+		user_params = params[:user]
+
+		user.first_name = user_params[:first_name]
+		user.email = user_params[:email]
+		user.password = user_params[:password]
+
+		user.save
+
+		redirect_to root_path
+	end
 end
