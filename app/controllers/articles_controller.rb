82cb01419c5c2b368c14bf58fcf650f8a1d27266
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-02-26 20:53:32 -0500

    styled_new_articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,53 @@
 class ArticlesController < ApplicationController
+
+	before_filter :load_article, :only => [:show, :update, :destroy]
+
+	def index
+		@articles = Article.all.order("Created_at DESC")
+	end
+
+	def show
+
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.create(article_params)
+		if @article.save
+			flash[:success] = "Article created!"
+			redirect_to articles_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @article.update(article_params)
+			flash[:success] = "Article updated!"
+			redirect_to articles_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @article.destroy
+			flash[:success] = "Article deleted"
+		else
+			flash[:error] = "Something went wrong!"
+		end
+		redirect_to articles_path
+	end
+
+	private
+	def articles_params
+		params.require(:article).oermit(:title, :body)
+	end
+
+	def load_article
+		@article = Article.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root "users#index"
+	root "static_pages#index"
 
-	resources :users
+	resources :static_pages, :articles
 end
